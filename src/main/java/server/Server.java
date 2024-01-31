bfdcad2479031a49eb4ddd72afd9979d499bce4d
Leon <leon.poellinger+git@gmail.com>
2024-01-31 12:50:35 +0100

    Update Server class thread safety and key handling
    The commit enhances thread safety in the Server class by synchronizing access to the shared keys set. It also refactors the way authentication keys are refreshed and checked. The possibility for race conditions and related bugs during key refreshing and checking is reduced by making sure these operations are thread-safe. Additionally, adjusted the sleep duration between readKeys operations.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,19 +8,27 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.Duration;
+import java.util.Set;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class Server {
-	private static HashSet<String> keys;
+	private static final Set<String> keys = Collections.synchronizedSet(new HashSet<>());
 	private static final String KEYS_PATH = "src/main/resources/authenticationKeys.txt";
-	public static void main(String[] args) {
+	private static final int SLEEP_MINS = 30;
 
-        // Create a new thread that continuously reads authentication keys.
-        new Thread(() -> {
-            while(true) {
-                readKeys();
-            }
-        }).start();
+	public static void main(String[] args) throws InterruptedException {
+
+		// Create a new thread that continuously reads authentication keys.
+		new Thread(() -> {
+			while(true) {
+				readKeys();
+			}
+		}).start();
+
+		Thread.sleep(500);
+		System.out.println("Waited");
+		printSet();
 
 		try (ServerSocket server = new ServerSocket(8080)) {
 
@@ -35,14 +43,23 @@ public class Server {
 
 	}
 
+	private static void printSet() {
+		synchronized (keys) {
+			for(String key : keys) System.out.println(key);
+		}
+	}
+
 	/**
 	 * Checks if the provided key exists in the set of authentication keys.
 	 *
 	 * @param key the key to check for existence
+	 *
 	 * @return true if the key exists, false otherwise
 	 */
-	boolean containsKey(String key) {
-		return keys.contains(key);
+	static boolean containsKey(String key) {
+		synchronized (keys) {
+			return keys.contains(key);
+		}
 	}
 
 	/**
@@ -50,21 +67,24 @@ public class Server {
 	 * If an IOException occurs while reading the file, a RuntimeException is thrown.
 	 * After reading the keys, the method sleeps for 30 minutes before finishing.
 	 *
-	 * @throws RuntimeException if an IOException occurs while reading the file
+	 * @throws RuntimeException     if an IOException occurs while reading the file
 	 * @throws InterruptedException if the thread is interrupted while sleeping
 	 */
 	@SneakyThrows
 	private static void readKeys() {
-		HashSet<String> temp = new HashSet<>();
+		Set<String> temp = new HashSet<>();
 		try (BufferedReader reader = new BufferedReader(new FileReader(KEYS_PATH))) {
 
 			String key;
-			while((key = reader.readLine()) != null) keys.add(key);
+			while((key = reader.readLine()) != null) temp.add(key);
 
 		} catch(IOException e) {
 			throw new RuntimeException(e);
 		}
-		keys = temp;
-		Thread.sleep(Duration.ofMinutes(30));
+		synchronized (keys) {
+			keys.clear();
+			keys.addAll(temp);
+		}
+		Thread.sleep(Duration.ofMinutes(SLEEP_MINS));
 	}
 }
