4f51b6728e019080190ed0485583d825bb9d20a0
Leon <leon.poellinger+git@gmail.com>
2024-01-31 12:22:53 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Default when creating
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+
+    // https://projectlombok.org/setup/gradle
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+val jarName = "ChatBoardServer"
+val jarVersion = "0.0.1"
+
+tasks.jar {
+
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+    archiveBaseName.set(jarName)
+    version = jarVersion
+
+    manifest.attributes["Main-Class"] = "server/Main"
+
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map { zipTree(it) }
+    from(dependencies)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "ChatBoardServer_new"
+
