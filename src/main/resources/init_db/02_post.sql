2e47ee8d3501d94250e6ea17dc6776651e366343
Leon <leon.poellinger+git@gmail.com>
2024-02-03 17:58:51 +0100

    Modify 'content' field type in 'post' table
    The change is made in 02_post.sql file where 'content' field type in 'post' table is changed from TEXT to VARCHAR(500). The adjustment aims to limit the size of the content entered for better database management.

--- src/main/resources/init_db/02_post.sql
+++ src/main/resources/init_db/02_post.sql
@@ -1,7 +1,7 @@
 CREATE TABLE post
 (
     id        SERIAL PRIMARY KEY,
-    content   TEXT NOT NULL,
+    content   VARCHAR(500) NOT NULL,
     author_id INTEGER REFERENCES "user" (id) NOT NULL,
     upvotes   text,
     downvotes text
