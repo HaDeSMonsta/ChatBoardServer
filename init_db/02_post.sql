982fc859ebd878cdc64da7d6a581e9eb975a9da9
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-02-05 14:09:50 +0100

    Reorganize SQL schemas and integrate Flyway for migration management
    All SQL schemas have been moved from the `init_db` folder into a single file in the Flyway migrations directory for better organization and scalability. Flyway has been added as a dependency in `build.gradle.kts` to handle database version control. Changes also reflect the removal of `init_db` folder reference in the Docker Compose file and `.dockerignore` document.

--- init_db/02_post.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE post
-(
-    id        SERIAL PRIMARY KEY,
-    content   VARCHAR(500) NOT NULL,
-    author_id INTEGER REFERENCES "user" (id) NOT NULL,
-    upvotes   text NOT NULL,
-    downvotes text NOT NULL
-);
