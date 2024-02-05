fa86c3b4980f214d1c1f3fb38f5dae60a54f3470
Leon <leon.poellinger+git@gmail.com>
2024-02-05 22:00:49 +0100

    Add UserController with showAllUsers endpoint
    A new controller 'UserController' has been created. This controller has a 'showAllUsers' endpoint that returns a sorted list of all users, with data like id, name, secNum, and blocked status. The sorting is performed based on user id.

---
+++ src/main/java/app/server/Web/UserController.java
@@ -0,0 +1,40 @@
+package app.server.Web;
+
+import app.database.user.User;
+import app.database.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping
+	public List<UserPDT> showAllUsers() {
+		List<User> posts = userService.getAllUsers();
+		return posts
+				.stream()
+				.map(user -> new UserPDT(
+						user.getId(),
+						user.getName(),
+						user.getSecNum(),
+						user.getBlocked()
+				))
+				.sorted(Comparator.comparing(UserPDT::id))
+				.toList();
+	}
+
+	public record UserPDT(long id, String name, int secNum, boolean blocked) {
+	}
+}
