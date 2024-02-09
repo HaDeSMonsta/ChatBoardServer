7413992d95e8bb5366f899844faa564949562a28
Leon <leon.poellinger+git@gmail.com>
2024-02-09 16:13:52 +0100

    Add NumStatsController for user and post statistics
    NumStatsController has been added to the project. This controller serves to extract and display user and post statistics using UserService and PostService. This controller will be available at the 'numStats' path.

---
+++ src/main/java/app/server/Web/NumStatsController.java
@@ -0,0 +1,34 @@
+package app.server.Web;
+
+import app.database.post.PostService;
+import app.database.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "numStats")
+public class NumStatsController {
+	private final UserService userService;
+	private final PostService postService;
+
+	@Autowired
+	public NumStatsController(UserService userService, PostService postService) {
+		this.userService = userService;
+		this.postService = postService;
+	}
+
+	@GetMapping
+	public numStatsPDT[] getNumStats() {
+		int userNums = userService.getAllUsers().size();
+		int postNums = postService.getAllPosts().size();
+		return new numStatsPDT[]{
+				new numStatsPDT("Usercount", userNums),
+				new numStatsPDT("Postcount", postNums)
+		};
+	}
+
+	public record numStatsPDT(String name, int count) {
+	}
+}
